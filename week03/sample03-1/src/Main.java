4e4970c9b5209de79183b0666ff0170a71e38ef1
aka99596-cmd <aka99596@gmail.com>
2026-09-18 14:13:29 +0900

    3주차 수업 내용

---
+++ week03/sample03-1/src/Main.java
@@ -0,0 +1,13 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+void main() {
+    boolean flag; //false;
+    byte value1 =127; // -128 ~ 127
+    short value2 = 32767;  //-32768 ~ + 32767
+    int value3 = 444444444;  //2의63승 ~ 2의63승 -1
+    long value4 = 4444444444L;  //long형은 뒤에 L이 들어가야된다.
+    double value5 = 4.55555555555555555555555555555;
+    float value6 = 4.555555555555555555555555555555f; // float은 뒤에 f를 붙어야 된다.
+    char value7 = 'A';  // char 문자
+    String value8 = "AB"; //String 문자열
+}
